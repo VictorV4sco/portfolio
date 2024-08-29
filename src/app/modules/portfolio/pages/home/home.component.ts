b656053fec4c6b86dd27f55a50297daeac9c690a
Victor Da Costa Vasco <victordcvasco@gmail.com>
2024-08-28 21:08:01 -0300

    Header Component

--- src/app/modules/portfolio/pages/home/home.component.ts
+++ src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+//components
+import { HeaderComponent } from '../../components/header/header.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
